99adbd5b57ea25fd61046397770c073e971ea580
Fiona Wilson <fionaw237@gmail.com>
2018-08-21 19:33:51 +0100

    Initial commit

---
+++ homework/specs/hero_spec.js
@@ -0,0 +1,28 @@
+const assert = require('assert');
+const Hero = require('../hero.js');
+
+let hero;
+
+describe('Hero', function(){
+
+  xit('should have a name', function(){
+
+  });
+
+  xit('should start with full health', function(){
+
+  })
+
+  xit('should have a favourite food', function(){
+
+  })
+
+  xit('should be able to say their name', function(){
+
+  })
+
+  xit('should have tasks to complete', function(){
+
+  })
+
+})
