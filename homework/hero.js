2a764e9bc7ff0569e989458acb1bf88e15ea1e5f
Fiona Wilson <fionaw237@gmail.com>
2018-08-21 19:46:35 +0100

    Minor change

--- homework/hero.js
+++ homework/hero.js
@@ -10,30 +10,3 @@ Hero.prototype.sayName = function(){
 }
 
 module.exports = Hero;
-// const Taxi = function(manufacturer, model, driver){
-//   this.manufacturer = manufacturer;
-//   this.model = model;
-//   this.driver = driver;
-//   this.passengers = [];
-// }
-//
-// Taxi.prototype.numberOfPassengers = function(){
-//   return this.passengers.length;
-// }
-//
-// Taxi.prototype.addPassenger = function(passengerName){
-//   this.passengers.push(passengerName);
-// }
-//
-// Taxi.prototype.removePassengerByName = function(passengerName){
-//   const index = this.passengers.indexOf(passengerName);
-//   this.passengers.splice(index, 1);
-//   // this.passengers = this.passengers.filter(i => i !== name)
-// }
-//
-// Taxi.prototype.removeAllPassengers = function(){
-//   this.passengers = [];
-//   //this.passengers.splice(0, this.numberOfPassengers())
-// }
-//
-// module.exports = Taxi;
