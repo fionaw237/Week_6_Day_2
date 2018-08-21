8eab98696ee95dc204bb6b203e0e644fd26b6f5e
Fiona Wilson <fionaw237@gmail.com>
2018-08-21 20:07:31 +0100

    Adds basic food object and tests

---
+++ homework/specs/food_spec.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+const Food = require('../food.js');
+
+let food;
+
+describe('Food', function(){
+  beforeEach(function(){
+    food = new Food('Pizza', 10)
+  });
+
+  it('should have a name', function(){
+    const actual = food.name
+    assert.strictEqual(actual, 'Pizza')
+  });
+
+  it('should have a replenishment value', function(){
+    const actual = food.replenishment
+    assert.strictEqual(actual, 10)
+  })
+
+})
